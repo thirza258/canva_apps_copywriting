9dde50a3b8c6eee5d8ed0f12f7e8a4637835e13e
thirzq <thirzahmad@gmail.com>
2024-07-29 17:59:23 +0700

    init

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,12 @@
-import { Button, Rows, Text } from "@canva/app-ui-kit";
+import { Button, Rows, Text, TextInput, Title } from "@canva/app-ui-kit";
 import { addNativeElement } from "@canva/design";
 import * as React from "react";
 import styles from "styles/components.css";
 
+
 export const App = () => {
+  const [input, setInput] = React.useState("");
+
   const onClick = () => {
     addNativeElement({
       type: "TEXT",
@@ -14,12 +17,20 @@ export const App = () => {
   return (
     <div className={styles.scrollContainer}>
       <Rows spacing="2u">
-        <Text>
-          To make changes to this app, edit the <code>src/app.tsx</code> file,
-          then close and reopen the app in the editor to preview the changes.
-        </Text>
+        <Title 
+        alignment="center"
+        size="small"
+        capitalization="default"
+        >Generate with Gemini</Title>
+        <TextInput
+        type="text"
+        name="input"
+        disabled={false}
+        onChange={(value: string) => setInput(value)}
+        />
+        <Text>{input}</Text>
         <Button variant="primary" onClick={onClick} stretch>
-          Do something cool
+          Generate
         </Button>
       </Rows>
     </div>
